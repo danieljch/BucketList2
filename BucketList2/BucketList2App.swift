4fb27b05b8987e623ba8f75a2d6d6fcc9b267925
Daniel JCH <danielj.callisayah@gmail.com>
2022-06-24 11:32:19 -0400

    Initial Commit

---
+++ BucketList2/BucketList2App.swift
@@ -0,0 +1,17 @@
+//
+//  BucketList2App.swift
+//  BucketList2
+//
+//  Created by Daniel Jesus Callisaya Hidalgo on 24/6/22.
+//
+
+import SwiftUI
+
+@main
+struct BucketList2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BucketList2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BucketList2
+//
+//  Created by Daniel Jesus Callisaya Hidalgo on 24/6/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
