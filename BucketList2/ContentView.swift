42a763bdd07f9dc4f07be282da8a819f2a7a0709
Daniel JCH <danielj.callisayah@gmail.com>
2022-06-28 11:41:56 -0400

    Using Touch ID and Face ID

--- BucketList2/ContentView.swift
+++ BucketList2/ContentView.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 import MapKit
+import LocalAuthentication
 
 struct Location: Identifiable {
     let id = UUID()
@@ -16,12 +17,13 @@ struct Location: Identifiable {
 
 struct ContentView: View {
     @State private var mapRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 51.5, longitude: -0.12), span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2))
+    @State private var isUnlocked = false
     let locations = [
         Location(name: "Buckingham Palace", coordinate: CLLocationCoordinate2D(latitude: 51.501, longitude: -0.141)),
         Location(name: "Tower of London", coordinate: CLLocationCoordinate2D(latitude: 51.508, longitude: -0.076))
     ]
     var body: some View {
-        NavigationView {
+       /* NavigationView {
         Map(coordinateRegion: $mapRegion, annotationItems: locations) {
             location in
             MapAnnotation(coordinate: location.coordinate) {
@@ -35,6 +37,14 @@ struct ContentView: View {
         }
         .navigationTitle("London Explorer")
         }
+        */
+        VStack {
+            if isUnlocked {
+                Text("Unlocked")
+            } else {
+                Text("Locked")
+            }
+        }
         
     }
     
@@ -44,6 +54,24 @@ struct ContentView: View {
 
         // just send back the first one, which ought to be the only one
         return paths[0]
+    }
+    func authenticate() {
+        let context = LAContext()
+        var error: NSError?
+        
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            let reason = "We need to unlock your Data."
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) {
+                success, authenticationError in
+                if success {
+                    isUnlocked = true
+                } else {
+                    
+                }
+            }
+        } else {
+            
+        }
     }
 }
 
